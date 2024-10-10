b6c736c1f2ef294ad03baec7ff6b67952a51ca57
Hamidou.Sow <hamidou.sow@hotmail.fr>
2024-10-10 11:04:43 +0200

    add and configure vueform

--- assurance-manager/src/main.ts
+++ assurance-manager/src/main.ts
@@ -5,9 +5,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router' 
+import vueformConfig from '../vueform.config';
+import Vueform from '@vueform/vueform'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(Vueform, vueformConfig)
 app.use(router)
+
 app.mount('#app')
--- assurance-manager/tailwind.config.js
+++ assurance-manager/tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html', 
-    './src/**/*.{vue,js,ts,jsx,tsx}'
+    // './index.html', 
+    // './src/**/*.{vue,js,ts,jsx,tsx}'
+    './vueform.config.js', // or where `vueform.config.js` is located. Change `.js` to `.ts` if required.
+    './node_modules/.pnpm/@vueform+vueform@*/node_modules/@vueform/vueform/themes/tailwind/**/*.vue',
+    './node_modules/.pnpm/@vueform+vueform@*/node_modules/@vueform/vueform/themes/tailwind/**/*.js',
   ],
   theme: {
     extend: {}
   },
   plugins: [
-    
+    '@vueform/vueform/tailwind',
   ]
 }
