2563bdb9055f091ab995d3adcffae7c882a1107d
Hamidou.Sow <hamidou.sow@hotmail.fr>
2024-10-12 11:37:39 +0200

    create member view

---
+++ src/views/MemberView.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+
+import { useMember } from '@/stores/memberStore';
+
+const memberStore = useMember()
+
+
+</script>
+
+<template>
+    <div>
+        {{ member.firstname + ' ' + member.lastname }}
+    </div>
+</template>
+
+
+
+<style></style>
